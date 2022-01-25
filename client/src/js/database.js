1d6a722fb39e200f426b9d1949b0cb3643b9d504
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2022-01-24 21:04:47 -0800

    working on database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,8 +16,8 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.log('Into Data Base');
   const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  const txt = jateDb.transaction('jate', 'readwrite');
+  const store = txt.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Your data has been saved to DB', result.value);
@@ -28,8 +28,8 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('Grabbing from DB');
   const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
+  const txt = jateDb.transaction('jate', 'readonly');
+  const store = txt.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   result
